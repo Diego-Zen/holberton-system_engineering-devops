28ac0eb2c4178574fa34ed8bdbd0964da335b297
Diego Monroy <difemo91@gmail.com>
2019-12-10 01:50:24 +0000

    [FIX] add the complete route for pkill command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # puppet manifest for killing a process called killmenow
 exec { 'process-kill'
-  command  => 'pkill killmenow',
+  command  => '/usr/bin/pkill killmenow',
   provider => 'shell'
 }
