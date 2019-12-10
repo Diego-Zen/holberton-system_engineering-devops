e2670e4ba889e6f81f98c8a674df61ab7b678269
Diego Monroy <difemo91@gmail.com>
2019-12-10 01:53:59 +0000

    [FEAT] addthe flag that matches the pattern

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # puppet manifest for killing a process called killmenow
 exec { 'process-kill'
-  command  => '/usr/bin/pkill killmenow',
+  command  => '/usr/bin/pkill -x killmenow',
   provider => 'shell'
 }
