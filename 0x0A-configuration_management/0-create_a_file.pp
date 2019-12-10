ec4667891c6ad169ec70dbd6f9bc18e11e8c314c
Diego Monroy <difemo91@gmail.com>
2019-12-10 00:43:15 +0000

    [FEAT] add puppet manifest for create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,8 @@
-#!/usr/bin/env bash
+# puppet manifest for creating a file in /tmp
+file { 'create a file':
+    path    => '/tmp/holberton',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
+}
