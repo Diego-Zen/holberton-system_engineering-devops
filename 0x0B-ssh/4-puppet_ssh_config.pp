fb8fef0207375d9945eb0060c3e468baebc56560
Diego Monroy <difemo91@gmail.com>
2019-12-11 03:01:04 +0000

    [FIX] change password auth to no

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,16 +1,16 @@
 # change client configuration file to connect a server without typing a password
 file { 'ssh-config':
-  ensure => present
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
 }
-
+# set password auth to no
 file_line { 'pass':
   path  => '/etc/ssh/ssh_config',
-  line  => 'PasswordAuthentication yes',
-  match => '^PasswordAuthentication*'
+  line  => 'PasswordAuthentication no',
+  match => '^PasswordAuthentication*',
 }
-
+# add identity for holberton key if not exist
 file_line { 'id':
   path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/holberton'
+  line => 'IdentityFile ~/.ssh/holberton',
 }
