202aed15a8c2e6635ce6b5829d1e78e0791d87b7
Diego Monroy <difemo91@gmail.com>
2020-02-06 05:09:57 +0000

    [FIX] add ':' to print outp

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
             done = done + 1
 
     print(
-        "Employee {} is done with tasks({}/{})"
+        "Employee {} is done with tasks({}/{}):"
         .format(
             json_data.get('name'),
             done, total
