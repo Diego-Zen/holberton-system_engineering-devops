fb3bec31b5ee525402195341a02f6b7156d06482
Diego Monroy <difemo91@gmail.com>
2020-02-07 23:53:19 +0000

    [FEAT] add function that queries the reddit API and returns number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+    Queries the reddit API and returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    headers = {'user-agent': 'API python reddit subscribers'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        return(0)
+    json_data = r.json()
+    return json_data['data']['subscribers']
