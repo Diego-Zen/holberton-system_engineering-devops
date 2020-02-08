27b6f33e98c4721452bd0497da19583859948d85
Diego Monroy <difemo91@gmail.com>
2020-02-08 00:24:20 +0000

    [FEAT] add top ten hot posts query API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+    Queries the reddit API and prints the titles of the first 10 hot posts
+"""
+import requests
+
+
+def top_ten(subreddit):
+    headers = {'user-agent': 'API python reddit subscribers'}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    r = requests.get(
+        url,
+        params={'limit': 10},
+        headers=headers,
+        allow_redirects=False
+    )
+
+    if r.status_code != 200:
+        return(0)
+    else:
+        json_data = r.json()
+        for title in json_data['data']['children']:
+            print(title['data'].get('title'))
