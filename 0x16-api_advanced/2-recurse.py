6ff0e22c291be4166bf26b98117c688fb846b433
Diego Monroy <difemo91@gmail.com>
2020-02-08 03:07:46 +0000

    [FEAT] add recursive function that queries all hot articles for a given subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+    Queries the reddit API and prints the titles of the first 10 hot posts
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    headers = {'user-agent': 'API python reddit subscribers'}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    r = requests.get(
+        url,
+        params={'after': after},
+        headers=headers,
+        allow_redirects=False
+    )
+
+    if r.status_code != 200:
+        return(None)
+    else:
+        json_data = r.json()
+        after = json_data['data'].get('after')
+        for title in json_data['data']['children']:
+            hot_list.append(title['data'].get('title'))
+        if after is None:
+            return(hot_list)
+        else:
+            return(recurse(subreddit, hot_list, after))
