c102c5daeae1442e65261051b63ac001050b7826
Diego Monroy <difemo91@gmail.com>
2020-02-28 03:00:22 +0000

    [FIX] script add '#' to the begining of the line

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # This manifest is to change the ULIMIT for nginx
 exec { 'comment-default-ulimit':
-  command => "sed '/ULIMIT=\"-n 15\"/ s/^/#/' /etc/default/nginx",
+  command => "sed -i '/ULIMIT=\"-n 15/ s/^/#/' /etc/default/nginx",
   path    => ['/bin/'],
 }
 exec { 'restart-nginx':
