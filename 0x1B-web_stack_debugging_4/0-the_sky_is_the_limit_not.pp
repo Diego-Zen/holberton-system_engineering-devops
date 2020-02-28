4d2f48186db6228bd5facb010407777cc734952f
Diego Monroy <difemo91@gmail.com>
2020-02-28 03:25:16 +0000

    [FIX] change line

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # This manifest is to change the ULIMIT for nginx
 exec { 'comment-default-ulimit':
-  command => "sed -i 's/15/4096/g' /etc/default/nginx",
+  command => "sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\" -n 4096\"/g' /etc/default/nginx",
   path    => ['/bin/'],
 }
 exec { 'restart-nginx':
