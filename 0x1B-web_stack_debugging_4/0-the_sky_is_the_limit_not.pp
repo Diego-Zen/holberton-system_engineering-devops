c25d08d316fd2d26d1881adf02ec01a4bd937ee5
Diego Monroy <difemo91@gmail.com>
2020-02-28 03:27:02 +0000

    [FIX] add sudo

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # This manifest is to change the ULIMIT for nginx
 exec { 'comment-default-ulimit':
-  command => "sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\" -n 4096\"/g' /etc/default/nginx",
+  command => "sudo sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\" -n 4096\"/' /etc/default/nginx",
   path    => ['/bin/'],
 }
 exec { 'restart-nginx':
   command  => 'sudo service nginx restart',
-  provider => shell
+  provider => shell,
 }
